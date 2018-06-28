12cf9d72d323ae02aaaeb8250d78656ccd66d8ca
Billy Hwang <drproy2k@gmail.com>
2018-06-28 21:14:25 +0900

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -19,5 +19,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),      # ^ 로 시작해서 $ 로 끝나는지 조사. 즉 아마것도 없는 경우에 해당. http://127.0.0.1:8000/ 으로 접속해 오는 경우를 의미
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),     # 브라우저에 http://127.0.0.1:8000/post/5/라고 입력하면, 장고는 post_detail 뷰를 찾아 매개변수 pk가 5인 값을 찾아 뷰로 전달
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
 
